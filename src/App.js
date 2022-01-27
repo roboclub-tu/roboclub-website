d699792fdeb0cdd182e2b0b4b34705679c68cc86
pete <pavlovskipetko@gmail.com>
2022-01-27 18:43:06 +0200

    Some improvements.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './styles/App.scss';
+import { AboutUs } from "./components/AboutUs/AboutUs";
+import { Alumni } from "./components/Alumni/Alumni";
+import { Header } from "./components/Header/Header";
 
 function App() {
   return (
     <div className="App">
-
+      {/* TODO: put all sections */}
+      <Header/>
+      <AboutUs />
+      <Alumni />
     </div>
   );
 }
---
+++ src/components/AboutUs/AboutUs.js
@@ -0,0 +1,10 @@
+import { render } from "@testing-library/react"
+import "./style.scss"
+
+export function AboutUs() {
+    return (
+        <div id="about-us">
+            <span>About us skeleton</span>
+        </div>
+    )
+}
