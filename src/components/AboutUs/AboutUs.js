33f027c4a7ab34bc5b2b14a33777e8141e10029b
pete <pavlovskipetko@gmail.com>
2022-02-10 19:08:48 +0200

    Test deploy last

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,7 +3,7 @@ import "./style.scss"
 export function AboutUs() {
     return (
         <div id="about-us">
-            <span>About us skeleton (test deploy 2 !!!)</span>
+            <span>About us skeleton (test deploy 3 !!!)</span>
         </div>
     )
 }
