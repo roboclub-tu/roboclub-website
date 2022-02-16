27b133372b85362cf65b68a552a174685256e16c
Miroslav Nikolov <mirro.nikolov@gmail.com>
2022-02-16 21:42:27 +0200

    Navigation bar buttons fixed href attribute

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -7,8 +7,8 @@ export function Header() {
   return <nav id="header">
     <img src={logo} id="logo" />
     <img src={fullLogo} id="full-logo"/>
-    <a href="about-us" id="first" className="links">ЗА НАС</a>
-    <a href="team" className="links">ЕКИП</a>
-    <a href="projects" className="links">ПРОЕКТИ</a>
+    <a href="#about-us" id="first" className="links">ЗА НАС</a>
+    <a href="#team" className="links">ЕКИП</a>
+    <a href="#projects" className="links">ПРОЕКТИ</a>
   </nav>;
 }
