07ca252007a07e80ce0de65d8d13995cd2401b3c
Miroslav Nikolov <mirro.nikolov@gmail.com>
2022-02-17 14:37:51 +0200

    Changed structure (#21)
    Changed components structure according to the design
    
    Co-authored-by: Petko (Pete) Pavlovski <pavlovskipetko@abv.bg>

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import './styles/App.scss';
 import { AboutUs } from "./components/AboutUs/AboutUs";
+//eslint-disable-next-line
 import { Alumni } from "./components/Alumni/Alumni";
 import { Header } from "./components/Header/Header";
+//eslint-disable-next-line
 import {Contacts} from "./components/Contacts/Contacts"
 import {Team} from "./components/Team/Team"
 import {Projects} from "./components/Projects/Projects"
-
+import {Welcome} from "./components/Welcome/Welcome"
 function App() {
   return (
     <div className="App">
       {/* TODO: put all sections */}
       <Header/>
-      <AboutUs />
+      <Welcome />
       <Team />
-      <Alumni />
+      <AboutUs />
       <Projects/>
-      <Contacts/>
     </div>
   );
 }
