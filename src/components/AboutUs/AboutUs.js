dae9eca9f340be2e9c017a9239cd1c3672b7cfee
pete <pavlovskipetko@gmail.com>
2022-02-18 20:59:20 +0200

    Staging test

--- src/components/AboutUs/AboutUs.js
+++ src/components/AboutUs/AboutUs.js
@@ -3,7 +3,7 @@ import "./AboutUs.scss"
 export function AboutUs() {
     return (
         <div id="about-us">
-            <span>About us skeleton (test deploy 3 !!!)</span>
+            <span>About us skeleton (test dev deploy)</span>
         </div>
     )
 }
