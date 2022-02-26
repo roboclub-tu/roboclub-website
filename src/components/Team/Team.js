5bd2ba9e242f5a1a290ed3ac860d928421c96155
Yancho Kostov <yankostov1@gmail.com>
2022-02-26 16:22:29 +0200

    Add text

--- src/components/Team/Team.js
+++ src/components/Team/Team.js
@@ -3,8 +3,13 @@ import waves from "../../assets/waves.svg"
 
 export function Team() {
     return <div id='team' className='parallax'>
-        {/* <div className='wavesContainer'> */}
-             <img src={waves} className="wavesImg" alt="waves" />
-        {/* </div> */}
-           </div>;
+        <div className='wavesContainer'>
+            <img src={waves} className="wavesImg" alt="waves" />
+            <div className='contentContainer'>
+                <h1>Нашият екип</h1>
+                <p>Exercitation consequat aute et exercitation minim nostrud officia ullamco. Reprehenderit irure nisi id dolore mollit pariatur reprehenderit. Ipsum fugiat nisi commodo do enim labore aliqua cupidatat consequat in ad. Incididunt aute laborum do et veniam sunt cillum deserunt Lorem sint dolore. Do officia ut in eu ipsum ad cillum nisi laborum enim qui sit excepteur.</p>
+                
+            </div>
+        </div>
+    </div>
 }
