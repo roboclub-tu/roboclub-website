3066120b297766b92a984076d1551e592bdca36e
Miroslav Nikolov <mirro.nikolov@gmail.com>
2022-03-10 22:38:08 +0200

    Change position of header links

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -14,9 +14,9 @@ export function Header() {
           <Link to="team" smooth={true} offset={-70} duration={750}> <img src={fullLogo} alt="logo" id="full-logo"/> </Link>     
         </div>
         <div id="header-links"> 
+          <Link to="team" smooth={true} offset={-95} duration={750}>ЕКИП</Link>
           <Link to="about-us" smooth={true} offset={-70} duration={750}> ЗА НАС </Link>
-          <Link to="team" smooth={true} offset={-70} duration={750}>ЕКИП</Link>
-          <Link to="projects" smooth={true} offset={-70} duration={750}>ПРОЕКТИ</Link>
+          <Link to="projects" smooth={true} offset={-90} duration={750}>ПРОЕКТИ</Link>
         </div>
       </div>
   </nav>;
